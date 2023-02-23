95963475384b28f18dda1e0a56b0b7abb3fadc09
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-02-23 16:28:09 +0100

    commit

--- src/compontents/MyCard.tsx
+++ src/compontents/MyCard.tsx
@@ -13,9 +13,11 @@ const MyCard = (props: MyCardProps) => {
   return (
     <Card className="mb-5">
       <Card.Img variant="top" src={props.imgUrl} style={{ height: "400px" }} />
-      <Card.Body className="p-4" style={{ height: "220px" }}>
+      <Card.Body className="p-4" style={{ height: "250px" }}>
         <Link to={"/news/" + props.id} style={{ textDecoration: "inherit" }}>
-          <Card.Title className="text-dark">{props.title}</Card.Title>
+          <Card.Title className="text-dark" style={{ fontSize: "1.7rem" }}>
+            {props.title}
+          </Card.Title>
         </Link>
         <Card.Text>{props.summary}</Card.Text>
       </Card.Body>
